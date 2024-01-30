de867aa56759152df94e9c46b26ba5deb61ac41e
l6s6 <59177217+l6s6@users.noreply.github.com>
2024-01-30 20:50:59 +0100

    commit for launch

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -1,202 +1,21 @@
-/*
 import React, { useState } from 'react';
 import Papa from 'papaparse';
 import Plot from 'react-plotly.js';
 
 const CsvPlotter = () => {
-  const [csvData, setCsvData] = useState({
-    uptime: [],
-    GNSS_PPS_Timestamp: [],
-  });
-
-  const handleFileUpload = (event) => {
-    const file = event.target.files[0];
-
-    Papa.parse(file, {
-      header: true,
-      delimiter: ';', // CSV-Trennzeichen
-      dynamicTyping: true,
-      //Die erste Zeilen überspringen bis Daten beginnen
-      beforeFirstChunk: (chunk) => {
-        const lines = chunk.split('\n');
-        const headerIndex = lines.findIndex((line) =>
-          line.includes('CSV_DATA_SECTION_BEGIN')
-        );
-        lines.splice(0, headerIndex + 1);
-        return lines.join('\n');
-      },
-      complete: (result) => {
-        console.log(result.data[0]);
-        const uptime = result.data.map((row) => row['Uptime [s]']);
-        const GNSS_PPS_Timestamp = result.data.map(
-          (row) => row['GNSS: PPS Timestamp [s]']
-        );
-
-        setCsvData({
-          uptime,
-          GNSS_PPS_Timestamp,
-        });
-      },
-      error: (error) => {
-        console.error('Fehler beim Parsen der CSV-Datei:', error);
-      },
-    });
+  const [yAxis, setYAxis] = React.useState('Temperature');
+  const handleChangeY = (newValue) => {
+    setYAxis(newValue);
   };
 
-  return (
-    <div>
-      <input type='file' onChange={handleFileUpload} />
-      {csvData.uptime.length > 0 && (
-        <Plot
-          data={[
-            {
-              x: csvData.uptime,
-              y: csvData.GNSS_PPS_Timestamp,
-              type: 'scatter',
-              mode: 'lines',
-              name: 'Temperature',
-            },
-            {
-              x: csvData.uptime,
-              y: csvData.GNSS_PPS_Timestamp,
-              type: 'scatter',
-              mode: 'lines',
-              name: 'Speed',
-            },
-          ]}
-          layout={{
-            width: 800,
-            height: 400,
-            title: 'CSV Daten Plot',
-            xaxis: { title: 'Time' },
-          }}
-        />
-      )}
-    </div>
-  );
-};
-
-export default CsvPlotter;
-*/
-
-/*
-import React, { useState } from 'react';
-import Papa from 'papaparse';
-import Plot from 'react-plotly.js';
-
-const CsvPlotter = () => {
-  const [csvData, setCsvData] = useState({
-    'Uptime [s]': [],
-    'GNSS: PPS Timestamp [s]': [],
-    'GNSS: UTC date time (ISO 8601)': [],
-    'GNSS: Fix type (0=no fix, others=fix)': [],
-    'GNSS: Latitude [degrees]': [],
-    'GNSS: Longitude [degrees]': [],
-    'GNSS: Positional dilution of precision': [],
-    'GNSS: Altitude [m] (above Mean Sea Level)': [],
-    'GNSS: Ground speed [km/h]': [],
-    'GNSS: Satellites in view': [],
-    'GNSS: Altitude [m] (above ellipsoid)': [],
-    'Temperature: Board [degC]': [],
-    'Temperature: Ext LM75 1 [degC]': [],
-    'Temperature: Ext MS8607 1 [degC]': [],
-    'Pressure: Ext MS8607 [hPa]': [],
-    'Humidity: Ext MS8607 [%]': [],
-    'Light Intensity: UVA index []': [],
-    'Volt: Supply Voltage [V]': [],
-    'Volt: 3.3V board voltage [V]': [],
-    'Volt: 5V board voltage [V]': [],
-    'Volt: Vin1 voltage [V]': [],
-    'Volt: Vin2 voltage [V]': [],
-    'Volt: Vin3 voltage [V]': [],
-    'IO: In 1 State': [],
-    'IO: In 1 Timestamp [s]': [],
-    'IO: In 2 State': [],
-    'IO: In 2 Timestamp [s]': [],
-    'IO: Out 1 State': [],
-    'IO: Out 1 Timestamp [s]': [],
-    'IO: Out 2 State': [],
-    'IO: Out 2 Timestamp [s]': [],
-  });
-
-  const handleFileUpload = (event) => {
-    const file = event.target.files[0];
-
-    Papa.parse(file, {
-      header: true,
-      delimiter: ';',
-      dynamicTyping: true,
-      complete: (result) => {
-        const headers = result.meta.fields || [];
-        const data = result.data;
-
-        const dataSectionIndex = data.findIndex(
-          (row) => row['Uptime [s]'] === 'CSV_DATA_SECTION_BEGIN'
-        );
-        const relevantData = data.slice(dataSectionIndex + 1);
-
-        const structuredData = relevantData.reduce((acc, row) => {
-          headers.forEach((header) => {
-            if (!acc[header]) {
-              acc[header] = [];
-            }
-            acc[header].push(row[header]);
-          });
-          return acc;
-        }, {});
-
-        setCsvData(structuredData);
-      },
-      error: (error) => {
-        console.error('Fehler beim Parsen der CSV-Datei:', error);
-      },
-    });
+  const [xAxis, setXAxis] = React.useState('Uptime');
+  const handleChangeX = (newValue) => {
+    setXAxis(newValue);
   };
 
-  return (
-    <div>
-      <input type='file' onChange={handleFileUpload} />
-      {csvData['Uptime [s]'] && (
-        <Plot
-          data={[
-            {
-              x: csvData['Uptime [s]'],
-              y: csvData['Temperature: Board [degC]'],
-              type: 'scatter',
-              mode: 'lines',
-              name: 'Temperature: Board [degC]',
-            },
-            {
-              x: csvData['Uptime [s]'],
-              y: csvData['GNSS: Ground speed [km/h]'],
-              type: 'scatter',
-              mode: 'lines',
-              name: 'GNSS: Ground speed [km/h]',
-            },
-            // Füge weitere Linien nach Bedarf hinzu
-          ]}
-          layout={{
-            width: 800,
-            height: 400,
-            title: 'CSV Daten Plot',
-            xaxis: { title: 'Uptime [s]' },
-          }}
-        />
-      )}
-    </div>
-  );
-};
-
-export default CsvPlotter;
-*/
-
-import React, { useState } from 'react';
-import Papa from 'papaparse';
-import Plot from 'react-plotly.js';
-
-const CsvPlotter = () => {
   const [csvData, setCsvData] = useState({
     Uptime: [],
+    Uptime_Min: [],
     GNSS_PPS_Timestamp: [],
     GNSS_UTC_date_time: [],
     GNSS_Fix_type: [],
@@ -205,6 +24,7 @@ const CsvPlotter = () => {
     GNSS_Positional_dilution_of_precision: [],
     GNSS_Altitude_above_Mean_Sea_Level: [],
     GNSS_Ground_speed: [],
+    Vertical_Speed: [],
     GNSS_Satellites_in_view: [],
     GNSS_Altitude_above_ellipsoid: [],
     Temperature_Board: [],
@@ -235,7 +55,6 @@ const CsvPlotter = () => {
     Max_Temperature_Ext_MS8607: 0, //Maximale Außentemperatur
     Min_Temperature_Ext_MS8607: 0, //Minimale Außentemperatur
     Max_GNSS_Altitude_above_Mean_Sea_Level: 0, //Maximale Höhe über Meeresspiegel
-    Max_GNSS_Altitude_above_ellipsoid: 0, //Maximale Höhe von Start
     Max_GNSS_Ground_speed: 0, //Maximale Geschwindigkeit
     Min_Pressure_Ext_MS8607: 0, //Minimaler Druck
     Max_Pressure_Ext_MS8607: 0, //Maximaler Druck
@@ -344,10 +163,18 @@ const CsvPlotter = () => {
         const IO_Out_2_Timestamp = result.data.map(
           (row) => row['IO: Out 2 Timestamp [s]']
         );
-        console.log(Temperature_Board);
+
+        const Vertical_Speed = calculateVelocity(
+          GNSS_Altitude_above_Mean_Sea_Level,
+          Uptime
+        );
+
+        const convertSecondsToMinutes = (seconds) => seconds / 60;
+        const Uptime_Min = Uptime.map(convertSecondsToMinutes);
 
         setCsvData({
           Uptime,
+          Uptime_Min,
           GNSS_PPS_Timestamp,
           GNSS_UTC_date_time,
           GNSS_Fix_type,
@@ -356,6 +183,7 @@ const CsvPlotter = () => {
           GNSS_Positional_dilution_of_precision,
           GNSS_Altitude_above_Mean_Sea_Level,
           GNSS_Ground_speed,
+          Vertical_Speed,
           GNSS_Satellites_in_view,
           GNSS_Altitude_above_ellipsoid,
           Temperature_Board,
@@ -391,10 +219,6 @@ const CsvPlotter = () => {
           GNSS_Altitude_above_Mean_Sea_Level.filter(
             (value) => typeof value === 'number' && !isNaN(value)
           );
-        const V_GNSS_Altitude_above_ellipsoid =
-          GNSS_Altitude_above_ellipsoid.filter(
-            (value) => typeof value === 'number' && !isNaN(value)
-          );
         const V_GNSS_Ground_speed = GNSS_Ground_speed.filter(
           (value) => typeof value === 'number' && !isNaN(value)
         );
@@ -412,9 +236,6 @@ const CsvPlotter = () => {
           Max_GNSS_Altitude_above_Mean_Sea_Level: Math.max(
             ...V_GNSS_Altitude_above_Mean_Sea_Level
           ),
-          Max_GNSS_Altitude_above_ellipsoid: Math.max(
-            ...V_GNSS_Altitude_above_ellipsoid
-          ),
           Max_GNSS_Ground_speed: Math.max(...V_GNSS_Ground_speed),
           Max_Pressure_Ext_MS8607: Math.max(...V_Pressure_Ext_MS8607),
           Min_Pressure_Ext_MS8607: Math.min(...V_Pressure_Ext_MS8607),
@@ -428,70 +249,230 @@ const CsvPlotter = () => {
     });
   };
 
+  const calculateVelocity = (heightData, timeData) => {
+    // Annahme: heightData und timeData sind Arrays der gleichen Länge
+    const velocityData = [];
+
+    for (let i = 1; i < heightData.length; i++) {
+      const heightChange = heightData[i] - heightData[i - 1];
+      const timeChange = timeData[i] - timeData[i - 1];
+      const velocity = heightChange / timeChange;
+      velocityData.push(velocity * 3.6);
+    }
+
+    return velocityData;
+  };
+
+  const xAxisTitle = {
+    title: {
+      text:
+        xAxis === 'Uptime'
+          ? 'Zeit [min]'
+          : xAxis === 'Height'
+          ? 'Höhe [m]'
+          : xAxis === 'Pressure'
+          ? 'Druck [hPa]'
+          : null,
+    },
+  };
+
+  const xAxisValue =
+    xAxis === 'Uptime'
+      ? csvData.Uptime_Min
+      : xAxis === 'Height'
+      ? csvData.GNSS_Altitude_above_Mean_Sea_Level
+      : xAxis === 'Pressure'
+      ? csvData.Pressure_Ext_MS8607
+      : null;
+
+  const yAxisTitle1 = {
+    title: {
+      text:
+        yAxis === 'Temperature'
+          ? 'Innentemperatur [C]'
+          : yAxis === 'Height'
+          ? 'Höhe [m]'
+          : yAxis === 'Speed'
+          ? 'Geschwindigkeit [km/h]'
+          : yAxis === 'Pressure'
+          ? 'Druck [hPa]'
+          : yAxis === 'Humidity'
+          ? 'Luftfeuchtigkeit [%]'
+          : yAxis === 'Light'
+          ? 'UV Index'
+          : null,
+    },
+  };
+  const yAxisTitle2 = {
+    title: {
+      text:
+        yAxis === 'Temperature'
+          ? 'Außentemperatur [C]'
+          : yAxis === 'Speed'
+          ? 'Steiggeschwindigkeit [km/h]'
+          : null,
+    },
+  };
+
+  const yAxisValue1 =
+    yAxis === 'Temperature'
+      ? csvData.Temperature_Board
+      : yAxis === 'Height'
+      ? csvData.GNSS_Altitude_above_Mean_Sea_Level
+      : yAxis === 'Speed'
+      ? csvData.GNSS_Ground_speed
+      : yAxis === 'Pressure'
+      ? csvData.Pressure_Ext_MS8607
+      : yAxis === 'Humidity'
+      ? csvData.Humidity_Ext_MS8607
+      : yAxis === 'Light'
+      ? csvData.Light_Intensity_UVA_index
+      : null;
+  const yAxisValue2 =
+    yAxis === 'Temperature'
+      ? csvData.Temperature_Ext_MS8607
+      : yAxis === 'Height'
+      ? csvData.GNSS_Altitude_above_Mean_Sea_Level
+      : yAxis === 'Speed'
+      ? csvData.Vertical_Speed
+      : yAxis === 'Pressure'
+      ? csvData.Pressure_Ext_MS8607
+      : yAxis === 'Humidity'
+      ? csvData.Humidity_Ext_MS8607
+      : yAxis === 'Light'
+      ? csvData.Light_Intensity_UVA_index
+      : null;
+
   return (
     <div>
       <input type='file' onChange={handleFileUpload} />
 
       {csvData.Uptime.length > 0 && (
         <div className='flex justify-around'>
-          <div className='h-96 bg-gray-100 rounded-2xl mt-20 justify-center px-8 py-4'>
+          <div className='bg-gray-100 rounded-2xl mt-20 justify-center px-8 py-4'>
             <p className='text-2xl font-bold mb-4'>Zusammenfassung</p>
-            <p>Minimale Innentemperatur: {summary.Min_Temperature_Board}</p>
-            <p>Maximale Innentemperatur: {summary.Max_Temperature_Board}</p>
+            <p>Minimale Innentemperatur: {summary.Min_Temperature_Board} [C]</p>
+            <p>Maximale Innentemperatur: {summary.Max_Temperature_Board} [C]</p>
+
             <p>
-              Minimale Außentemperatur: {summary.Min_Temperature_Ext_MS8607}
+              Minimale Außentemperatur: {summary.Min_Temperature_Ext_MS8607} [C]
             </p>
             <p className='mb-4'>
-              Maximale Außentemperatur: {summary.Max_Temperature_Ext_MS8607}
+              Maximale Außentemperatur: {summary.Max_Temperature_Ext_MS8607} [C]
             </p>
-            <p className='mb-4'>
+            <p>
               Maximale Höhe (Meeresspiegel):{' '}
-              {summary.Max_Temperature_Ext_MS8607}
+              {summary.Max_GNSS_Altitude_above_Mean_Sea_Level} [m]
             </p>
             <p className='mb-4'>
-              Maximale Höhe (Start): {summary.Max_Temperature_Ext_MS8607}
+              Maximale Geschwindigkeit: {summary.Max_GNSS_Ground_speed} [km/h]
+            </p>
+            <p>Maximaler Druck: {summary.Max_Pressure_Ext_MS8607} [hPa]</p>
+            <p className='mb-4'>
+              Minimaler Druck: {summary.Min_Pressure_Ext_MS8607} [hPa]
+            </p>
+
+            <p>
+              Maximale Luftfeuchtigkeit: {summary.Max_Humidity_Ext_MS8607} [%]
             </p>
             <p className='mb-4'>
-              Minimaler Druck: {summary.Max_Temperature_Ext_MS8607}
-            </p>
-            <p className='mb-4'>
-              Maximaler Druck: {summary.Max_Temperature_Ext_MS8607}
-            </p>
-            <p className='mb-4'>
-              Maximale Außentemperatur: {summary.Max_Temperature_Ext_MS8607}
-            </p>
-            <p className='mb-4'>
-              Maximale Außentemperatur: {summary.Max_Temperature_Ext_MS8607}
+              Minimale Luftfeuchtigkeit: {summary.Min_Humidity_Ext_MS8607} [%]
             </p>
           </div>
-
-          <Plot
-            data={[
-              {
-                x: csvData.Uptime,
-                y: csvData.Temperature_Board,
-                type: 'scatter',
-                mode: 'lines',
-                name: 'Temperature_Board',
-              },
-              {
-                x: csvData.Uptime,
-                y: csvData.Temperature_Ext_MS8607,
-                type: 'scatter',
-                mode: 'lines',
-                name: 'Temperature_Ext_MS8607',
-              },
-            ]}
-            layout={{
-              width: 1000,
-              height: 500,
-              title: 'Wetterbalon Daten',
-              xaxis: { title: 'Time' },
-            }}
-          />
+          <div className='flex flex-col justify-start'>
+            <Plot
+              data={[
+                {
+                  x: xAxisValue,
+                  y: yAxisValue1,
+                  type: 'scatter',
+                  mode: 'lines',
+                  name: yAxisTitle1,
+                },
+                {
+                  x: xAxisValue,
+                  y: yAxisValue2,
+                  type: 'scatter',
+                  mode: 'lines',
+                  name: yAxisTitle2,
+                },
+              ]}
+              layout={{
+                width: 1000,
+                height: 500,
+                title: 'Wetterbalon Daten',
+                xaxis: xAxisTitle,
+                yaxis: {
+                  title: {
+                    text: 'Temperatur [°C]',
+                  },
+                },
+              }}
+            />
+            <div className='flex gap-4'>
+              <span>y-Achse: </span>
+              <Checkbox
+                label='Temperatur'
+                value={yAxis === 'Temperature'}
+                onChange={() => handleChangeY('Temperature')}
+              />
+              <Checkbox
+                label='Höhe'
+                value={yAxis === 'Height'}
+                onChange={() => handleChangeY('Height')}
+              />
+              <Checkbox
+                label='Geschwindigkeit'
+                value={yAxis === 'Speed'}
+                onChange={() => handleChangeY('Speed')}
+              />
+              <Checkbox
+                label='Druck'
+                value={yAxis === 'Pressure'}
+                onChange={() => handleChangeY('Pressure')}
+              />
+              <Checkbox
+                label='Luftfeuchtigkeit'
+                value={yAxis === 'Humidity'}
+                onChange={() => handleChangeY('Humidity')}
+              />
+              <Checkbox
+                label='Lichtintensität'
+                value={yAxis === 'Light'}
+                onChange={() => handleChangeY('Light')}
+              />
+            </div>
+            <div className='flex gap-4'>
+              <span>x-Achse: </span>
+              <Checkbox
+                label='Zeit'
+                value={xAxis === 'Uptime'}
+                onChange={() => handleChangeX('Uptime')}
+              />
+              <Checkbox
+                label='Höhe'
+                value={xAxis === 'Height'}
+                onChange={() => handleChangeX('Height')}
+              />{' '}
+              <Checkbox
+                label='Druck'
+                value={xAxis === 'Pressure'}
+                onChange={() => handleChangeX('Pressure')}
+              />
+            </div>
+          </div>
         </div>
       )}
     </div>
+  );
+};
+
+const Checkbox = ({ label, value, onChange }) => {
+  return (
+    <label>
+      <input type='checkbox' checked={value} onChange={onChange} />
+      {label}
+    </label>
   );
 };
 
